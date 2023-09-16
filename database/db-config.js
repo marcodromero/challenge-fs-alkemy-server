f6562c15862f2beda6ac383532c626fcec4255a9
Marco Romero <marcod.romero@gmail.com>
2023-09-16 12:15:44 -0300

    add environment variables to database connection

--- database/db-config.js
+++ database/db-config.js
@@ -1,8 +1,14 @@
-const {Sequelize} = require('sequelize');
+const { Sequelize } = require("sequelize");
 
-const db = new Sequelize('personal_budget', 'root', '', {
-    host: 'localhost',
-    dialect: 'mysql',
-});
+const db = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "mysql",
+  }
+);
 
 module.exports = db;
